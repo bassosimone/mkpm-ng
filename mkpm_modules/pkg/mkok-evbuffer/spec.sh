d6fbc8b08dbfffddd0e8327d17f9df5058a269ca
Simone Basso <bassosimone@gmail.com>
2016-01-11 00:11:40 +0100

    Add mkok-evbuffer mkok-evutil

---
+++ mkpm_modules/pkg/mkok-evbuffer/spec.sh
@@ -0,0 +1,3 @@
+pkg_name=mkok-evbuffer
+pkg_repo=https://github.com/bassosimone/$pkg_name
+pkg_deps="Catch mkok-base libevent"
---
+++ mkpm_modules/pkg/mkok-evutil/spec.sh
@@ -0,0 +1,3 @@
+pkg_name=mkok-evutil
+pkg_repo=https://github.com/bassosimone/$pkg_name
+pkg_deps="Catch mkok-base libevent"
